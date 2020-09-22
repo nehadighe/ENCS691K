2fb1eab8b15084002c53b99eeaad6505564cccf8
ELCHOCO <edgardojesus16@gmail.com>
2020-09-22 12:54:00 -0400

    added vuetify framework, set up the basics for the login and sign up pages

---
+++ front/src/components/Auth.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill-height">
+    <v-row>
+      <v-col class>
+        <div class="d-flex justify-center align-center">
+          <v-card width="400" class="card-signing elevation-2">
+            
+          </v-card>
+        </div>
+      </v-col>
+      <!-- Banner -->
+      <!-- <v-snackbar :color="bannerType" :timeout="timeout" v-model="alert">
+        {{ bannerText }}
+        <v-btn color="white" text @click="alert = false">
+          <v-icon>mdi-window-close</v-icon>
+        </v-btn>
+      </v-snackbar>-->
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-signing {
+  padding: 30px;
+}
+</style>
---
+++ front/src/components/SignUp.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-container class="fill-height">
+    <v-row>
+      <v-col class>
+        <div class="d-flex justify-center align-center">
+          <!-- <v-card width="auto" class="card-signing elevation-2"> -->
+          <v-card width="800" class="card-signing elevation-2">
+            <h1 class="pb-5 text-center">Sign Up</h1>
+            <v-form v-model="valid">
+              <v-row>
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    :color="themeColor"
+                    v-model="username"
+                    :rules="rules"
+                    autocomplete="username"
+                    name="signin-username"
+                    required
+                    label="Username"
+                    filled
+                  ></v-text-field>
+                </v-col>
+                <!-- email -->
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    :color="themeColor"
+                    v-model="username"
+                    :rules="rules"
+                    autocomplete="username"
+                    name="signin-username"
+                    required
+                    label="Email"
+                    filled
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <!-- first name -->
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    :color="themeColor"
+                    v-model="username"
+                    :rules="rules"
+                    autocomplete="username"
+                    name="signin-username"
+                    required
+                    label="First Name"
+                    filled
+                  ></v-text-field>
+                </v-col>
+                <!-- last name -->
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    :color="themeColor"
+                    v-model="username"
+                    :rules="rules"
+                    autocomplete="username"
+                    name="signin-username"
+                    required
+                    label="Last Name"
+                    filled
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <!-- password -->
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    v-model="pass"
+                    :color="themeColor"
+                    :append-icon="eye ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="eye ? 'text' : 'password'"
+                    :rules="rules"
+                    autocomplete="new-password"
+                    required
+                    filled
+                    name="signin-password"
+                    label="Password"
+                    @click:append="eye = !eye"
+                    @keyup.enter="login"
+                  ></v-text-field>
+                </v-col>
+                <!-- confirm password -->
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    v-model="pass"
+                    :color="themeColor"
+                    :append-icon="eye ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="eye ? 'text' : 'password'"
+                    :rules="rules"
+                    autocomplete="new-password"
+                    required
+                    filled
+                    name="signin-password"
+                    label="Confirm Password"
+                    @click:append="eye = !eye"
+                    @keyup.enter="login"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" class="d-flex justify-center">
+                  <!-- <v-col cols="12" md="6" class="d-flex justify-center justify-md-end"> -->
+                  <v-btn
+                    v-if="!requestLoading"
+                    :style="valid ? {transition: `0.3s ease`} : null"
+                    :color="valid ? themeColor : null"
+                    :class="[valid ? `white--black` : '']"
+                    @click="login"
+                  >Sign Up</v-btn>
+                </v-col>
+                <!-- <v-col cols="12" md="6" class="d-flex justify-center justify-md-start">
+                  <v-btn
+                    v-if="!requestLoading"
+                    :style="valid ? {transition: `0.3s ease`} : null"
+                    :color="valid ? themeColor : null"
+                    :class="[valid ? `white--black` : '']"
+                    @click="login"
+                  >Login</v-btn>
+                </v-col>-->
+              </v-row>
+              <!-- Conditional styling -->
+              <!-- <v-progress-circular
+                  v-if="requestLoading"
+                  :size="20"
+                  :color="themeColor"
+                  indeterminate
+              ></v-progress-circular>-->
+            </v-form>
+          </v-card>
+        </div>
+      </v-col>
+      <!-- Banner -->
+      <!-- <v-snackbar :color="bannerType" :timeout="timeout" v-model="alert">
+        {{ bannerText }}
+        <v-btn color="white" text @click="alert = false">
+          <v-icon>mdi-window-close</v-icon>
+        </v-btn>
+      </v-snackbar>-->
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-signing {
+  padding: 30px;
+}
+</style>
