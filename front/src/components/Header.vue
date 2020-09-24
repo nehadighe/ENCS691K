ff2cf5c6add01b58b6f503d2f245d1cffa70ce45
ELCHOCO <edgardojesus16@gmail.com>
2020-09-24 11:30:51 -0400

    modified header to reflect Concordia logo

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,37 +1,53 @@
 <template>
-  <v-app-bar color="primary" dark>
-    <div class="d-flex align-center">
-      <v-img
+  <v-app-bar :color="headerColor" dark>
+    <div class="d-flex align-center justify-start">
+      <div>
+        <v-img
+          alt="Concordia Logo"
+          class="shrink mr-2"
+          contain
+          src="https://encs691k-assets.s3.amazonaws.com/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+      <div class="">
+        <p class="red-font text-left mb-0">Concordia</p>
+        <p class="red-font text-left mb-0">ENCS691K</p>
+      </div>
+      <!-- <v-img
         alt="Vuetify Logo"
         class="shrink mr-2"
         contain
         src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
         transition="scale-transition"
         width="40"
-      />
+      />-->
+      <!-- <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />-->
 
-      <v-img
+      <!-- <v-img
         alt="Vuetify Name"
         class="shrink mt-1 hidden-sm-and-down"
         contain
         min-width="100"
         src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
         width="100"
-      />
+      />-->
     </div>
 
     <v-spacer></v-spacer>
 
-    <v-btn
-      text
-      @click="authModeLocal('signup')"
-    >
+    <v-btn text @click="authModeLocal('signup')">
       <span class="mr-2">Sign Up</span>
     </v-btn>
-    <v-btn
-      text
-      @click="authModeLocal('login')"
-    >
+    <v-btn text @click="authModeLocal('login')">
       <span class="mr-2">Login</span>
     </v-btn>
   </v-app-bar>
@@ -42,11 +58,13 @@ import { mapActions } from "vuex";
 
 export default {
   name: "Header",
-  data: () => ({}),
+  data: () => ({
+    headerColor: "#CDB394"
+  }),
   methods: {
     ...mapActions(["authMode"]),
     authModeLocal(event) {
-    //   console.log("verifying event", event);
+      //   console.log("verifying event", event);
       this.authMode(event);
     }
   }
@@ -55,4 +73,6 @@ export default {
 
 <style>
 @import "../assets/line.css";
+@import "../assets/colors.css";
+@import "../assets/font.css";
 </style>
--- git.sh
+++
@@ -1,8 +0,0 @@
-echo "# ENCS691K" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin git@github.com:nehadighe/ENCS691K.git
-git push -u origin master
-                
