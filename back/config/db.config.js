28292f9345620d6895274f806217cfee3888be2c
nehadighe <neha.dighe18@gmail.com>
2020-10-02 17:14:06 -0400

    check

--- back/config/db.config.js
+++ back/config/db.config.js
@@ -12,16 +12,3 @@ const pool = createPool({
 module.exports = pool;
 
 
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "123456",
-//     DB: "testdb",
-//     dialect: "mysql",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   };
